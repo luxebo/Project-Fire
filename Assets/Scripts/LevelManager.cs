5ad4fcd7e33f5d0e0b05d23c0b2f0ebe4da460e7
Michael Yuen <34826088+luxebo@users.noreply.github.com>
2019-06-19 16:26:36 -0700

    menu

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    //public GameObject startButton;
+    //public GameObject exitButton;
+
+    // Update is called once per frame
+    void Update (){
+    }
+
+    public void sceneChange(string scene)
+    {
+        if (scene == "Exit")
+        {
+            Application.Quit();
+        }
+        SceneManager.LoadScene(scene);
+    }
+}
