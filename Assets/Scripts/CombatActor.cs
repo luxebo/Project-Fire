17eb9e0baaeb9bb4bdc47d672d23bcc1dee4bc71
Lawrence Dang <lawrend1@uci.edu>
2019-06-26 23:05:28 -0700

    Added basic HP/damage scripts.

---
+++ Assets/Scripts/CombatActor.cs
@@ -0,0 +1,36 @@
+﻿// A CombatActor is an object that has HP.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatActor : MonoBehaviour {
+    public int health;
+    private bool alive;
+	// Use this for initialization
+	void Start () {
+        alive = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (alive == false)
+        {
+            die();
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (health <= 0)
+        {
+            alive = false;
+        }
+    }
+
+    // Derived classes can override; for example, might not want to destroy the player object
+    void die()
+    {
+        print("CombatActor died\n");
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Hitbox.cs
@@ -0,0 +1,26 @@
+﻿// Must be attached to a gameObject with a collider; damages CombatActors that collide.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour {
+    public int damage;
+    public bool canHurtSelf;
+
+    // Damage enemies that enter this hitbox. May want to change to OnTriggerStay if
+    // we want hitboxes with continuous damage.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!canHurtSelf && other.gameObject == this.gameObject)
+        {
+            return;
+        }
+        CombatActor enemy = other.gameObject.GetComponent<CombatActor>();
+        if (enemy != null)
+        {
+            enemy.health -= damage;
+            // for debugging
+            print(enemy.health);
+        }
+    }
+}
