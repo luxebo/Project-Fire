a3ca56a3e48da84f48899615d8899553d0a85fae
Lawrence Dang <lawrend1@uci.edu>
2019-06-27 18:01:53 -0700

    Added hitbox prefab, can set hitbox lifetime

--- Assets/Scripts/Hitbox.cs
+++ Assets/Scripts/Hitbox.cs
@@ -4,8 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hitbox : MonoBehaviour {
-    public int damage;
+    public float damage;
+    public float lifetimeSeconds; // How many seconds will this hitbox last? (Set negative if never disappears)
     public bool canHurtSelf;
+    private int lifetimeUpdates; // How many updates will this last? (rounded up)
+
+    private void Start()
+    {
+        if(lifetimeSeconds < 0)
+        {
+            lifetimeUpdates = -1;
+        }
+        else
+        {
+            lifetimeUpdates = Mathf.CeilToInt(lifetimeSeconds / Time.fixedDeltaTime);
+        }
+        
+    }
 
     // Damage enemies that enter this hitbox. May want to change to OnTriggerStay if
     // we want hitboxes with continuous damage.
@@ -18,9 +33,22 @@ public class Hitbox : MonoBehaviour {
         CombatActor enemy = other.gameObject.GetComponent<CombatActor>();
         if (enemy != null)
         {
-            enemy.health -= damage;
+            enemy.health -= Mathf.FloorToInt(damage);
             // for debugging
             print(enemy.health);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (lifetimeUpdates > 0)
+        {
+            lifetimeUpdates--;
+        }
+        
+        if (lifetimeUpdates == 0)
+        {
+            Destroy(this.gameObject);
         }
     }
 }
