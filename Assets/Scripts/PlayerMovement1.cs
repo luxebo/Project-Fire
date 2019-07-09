d02c8e1769a066497d0ff5811b6f18f824640d9a
Lawrence Dang <lawrend1@uci.edu>
2019-07-08 18:25:50 -0700

    Added option for camera-relative movement, rotation control

--- Assets/Scripts/PlayerMovement1.cs
+++ Assets/Scripts/PlayerMovement1.cs
@@ -8,36 +8,71 @@ public class PlayerMovement1 : MonoBehaviour
 
     public float moveSpeed;
     public float rotateSpeed;
+    public bool cameraRelative;
     private Rigidbody myRigidbody;
 
-    private Vector2 moveInput;
-    private Vector2 moveVelocity;
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    private float rotationInput;
 
     // Use this for initialization
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"),  Input.GetAxisRaw("Vertical"));
-        moveVelocity = Vector2.ClampMagnitude(moveInput * moveSpeed, moveSpeed); // Ensure that velocity is no greater than moveSpeed
+        // movement
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveDirection = moveInput;
+        // Camera relative movement
+        if (cameraRelative)
+        {
+            Camera camera = Camera.main;
+
+            Vector3 camForward = camera.transform.forward;
+            Vector3 camRight = camera.transform.right;
+            // Project camera onto horizontal plane
+            camForward.y = 0;
+            camRight.y = 0;
+            camForward = camForward.normalized;
+            camRight = camRight.normalized;
+
+            moveDirection = camForward * moveInput.z + camRight * moveInput.x;
+        }
+        moveVelocity = Vector3.ClampMagnitude(moveDirection * moveSpeed, moveSpeed); // Ensure that velocity is no greater than moveSpeed
+
+        /**
         if (Input.GetAxisRaw("Horizontal") >= 1 && Input.GetAxisRaw("Vertical") >= 1)
         {
-            moveInput = new Vector2(Input.GetAxisRaw("Horizontal") / Mathf.Sqrt(2), Input.GetAxisRaw("Vertical") /Mathf.Sqrt(2));
+            moveInput = new Vector3(Input.GetAxisRaw("Horizontal") / Mathf.Sqrt(2),0, Input.GetAxisRaw("Vertical") /Mathf.Sqrt(2));
         }
-        if (moveInput != Vector2.zero)
+        */
+        if (moveInput != Vector3.zero)
         {
             NavMeshAgent player = GetComponent<NavMeshAgent>();
             player.isStopped = true;
         }
+
+        // Rotation
+        rotationInput = Input.GetAxisRaw("Horizontal2");
+        
+
     }
 
     void FixedUpdate() {
-        transform.Translate(moveVelocity[0], 0, moveVelocity[1]);
+        // Set relative to Space.world for movement independent of rotation.
+        // Set relative to Space.Self for movement based on rotation.
+        transform.Translate(moveVelocity, Space.World);
         myRigidbody.velocity = moveVelocity;
+
+        // apply rotation
+        transform.Rotate(transform.up * rotationInput * rotateSpeed);
+        
+
         // Bugged, idk if we need rotation in the direction the player moves. Also is hard to test with cube.
         // Should copy functionality of the navmesh (right click to move).
         /*
