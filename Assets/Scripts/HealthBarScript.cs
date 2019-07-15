84b198f458033a1399af1d3358d30b78b2bea843
Lawrence Dang <lawrend1@uci.edu>
2019-07-15 16:03:58 -0700

    Added health bar

---
+++ Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour {
+
+    public GameObject player;
+    private CombatActor player_ca;
+    private Slider bar;
+	// Use this for initialization
+	void Start () {
+        player_ca = player.GetComponent<CombatActor>();
+        bar = gameObject.GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bar.value = player_ca.Health;
+	}
+}
