a031525c139e9728d3dfd3d84dae291d27d86e2a
Michael Yuen <34826088+luxebo@users.noreply.github.com>
2019-07-15 00:40:03 -0700

    hotkeys pt2 (saving them in file)

--- Assets/Scripts/Hotkeys.cs
+++ Assets/Scripts/Hotkeys.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,8 +10,18 @@ public class Hotkeys : MonoBehaviour {
     // Use this for initialization
     Text txt;
     Button pressable;
+    HotkeysSettings hk;
     private bool editable = false;
 	void Start () {
+        hk = loadHotkeys();
+        GameObject buttonObj = GameObject.Find("Buttons");
+        Transform buttons = buttonObj.transform;
+        for (int i = 0; i < buttons.childCount; ++i)
+        {
+            Transform but = buttons.transform.GetChild(i);
+            Text txt2 = but.GetComponentInChildren<Text>();
+            txt2.text = hk.hotkeys[i];
+        }
     }
 	
 	// Update is called once per frame
@@ -24,6 +36,7 @@ public class Hotkeys : MonoBehaviour {
                     int check = checkSameKey(vKey);
                     if (check == -1)
                     {
+                        hk.changeHotkey(txt.text, vKey.ToString());
                         txt.text = vKey.ToString();
                     }
                     else
@@ -31,6 +44,7 @@ public class Hotkeys : MonoBehaviour {
                         Transform buttons = pressable.transform.parent;
                         Transform but = buttons.transform.GetChild(check);
                         Text txt2 = but.GetComponentInChildren<Text>();
+                        hk.swapHotkeys(txt.text, txt2.text, vKey.ToString());
                         txt2.text = txt.text;
                         txt.text = vKey.ToString();
                     }
@@ -39,6 +53,7 @@ public class Hotkeys : MonoBehaviour {
                 {
                     editable = false;
                     pressable = null;
+                    saveHotkeys(hk);
                     break;
                 }
             }
@@ -68,5 +83,42 @@ public class Hotkeys : MonoBehaviour {
             }
         }
         return -1;
+    }
+
+    public static void saveHotkeys (HotkeysSettings hk)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/hotkeys";
+        FileStream stream = new FileStream(path, FileMode.Create);
+        formatter.Serialize(stream, hk);
+        stream.Close();
+    }
+
+    public static HotkeysSettings loadHotkeys()
+    {
+        string path = Application.persistentDataPath + "/hotkeys";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            HotkeysSettings hk = formatter.Deserialize(stream) as HotkeysSettings;
+            stream.Close();
+            return hk;
+        }
+        else //file not created yet
+        {
+            GameObject buttonObj = GameObject.Find("Buttons");
+            Transform buttons = buttonObj.transform;
+            string[] hotkeys = new string[buttons.childCount];
+            for (int i = 0; i < buttons.childCount; i++)
+            {
+                Transform but = buttons.transform.GetChild(i);
+                string txt2 = but.GetComponentInChildren<Text>().text;
+                hotkeys[i] = txt2;
+            }
+            HotkeysSettings hk = new HotkeysSettings(hotkeys, buttons.childCount);
+            saveHotkeys(hk);
+            return hk;
+        }
     }
 }
