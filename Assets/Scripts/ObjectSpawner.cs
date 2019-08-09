65ace695c34135d15faec17aa38f19b9928665ed
Lawrence Dang <lawrend1@uci.edu>
2019-08-08 18:03:52 -0700

    Proximity triggers

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -7,7 +7,7 @@ public class ObjectSpawner : MonoBehaviour {
     public string objectTag;
 
     [SerializeField]
-    private float spawnFrequency;
+    private float spawnFrequency; // If negative, never automatically spawn.
     [SerializeField]
     private int spawnLimit;
     private int frequencyInUpdates;
@@ -23,18 +23,25 @@ public class ObjectSpawner : MonoBehaviour {
         {
             throw new UnityException(string.Format("No object with the tag {0} exists in the pool", objectTag));
         }
-        frequencyInUpdates = Mathf.CeilToInt(spawnFrequency / Time.fixedDeltaTime);
+        frequencyInUpdates = spawnFrequency >= 0 ? Mathf.CeilToInt(spawnFrequency / Time.fixedDeltaTime):-1;
+        updatesSinceLastSpawn = frequencyInUpdates;
 	}
 
+    // Reset spawn timer
+    public void resetTimer()
+    {
+        updatesSinceLastSpawn = frequencyInUpdates;
+    }
+
     // Spawn a new object. Return true if successful, else false
-    bool spawn()
+    public bool spawn()
     {
         GameObject newObj = ObjectPooler.objectPool.getPooledObject(objectTag);
         if (newObj != null)
         {
             newObj.SetActive(true);
             newObj.transform.SetParent(gameObject.transform, true);
-            newObj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            //newObj.GetComponent<Rigidbody>().velocity = Vector3.zero;
             newObj.transform.localPosition = Vector3.zero;
             newObj.transform.SetParent(gameObject.transform.parent, true);
             numSpawned++;
@@ -44,7 +51,7 @@ public class ObjectSpawner : MonoBehaviour {
     }
 	
 	void FixedUpdate () {
-		if(numSpawned < spawnLimit)
+		if(frequencyInUpdates != -1 && numSpawned < spawnLimit)
         {
             if(updatesSinceLastSpawn == frequencyInUpdates)
             {
